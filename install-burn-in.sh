177c5527bbde9f436d801f685040502112d2d0c6
Castulo J. Martinez <castulo.martinez@intel.com>
2016-06-21 14:10:35 -0700

    Fix the installation of the plugin
    When installing the plugin, if it is installed using "pip install
    my_plugin" the plugin fails at run time, this commit changes the
    way the plugin is being installed by using "pip install -e my_plugin".

--- install-burn-in.sh
+++ install-burn-in.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd ~
 git clone https://github.com/CasJ/burn-in-tests-poc
-pip install burn-in-tests-poc/
+pip install -e burn-in-tests-poc/
 
 cd /opt/stack/new/tempest
 ostestr --regex burn-in 
