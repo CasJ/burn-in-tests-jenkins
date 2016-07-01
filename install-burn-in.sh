d97993018bafae35403a9030900f6dfba46f26f5
Castulo J. Martinez <castulo.martinez@intel.com>
2016-07-01 09:50:26 -0700

    Redirect tempest execution output to a local file
    This commit saves the tempest execution results into a local file
    called tempest_output

--- install-burn-in.sh
+++ install-burn-in.sh
@@ -4,4 +4,4 @@ git clone https://github.com/CasJ/burn-in-tests-poc
 pip install -e burn-in-tests-poc/
 
 cd /opt/stack/new/tempest
-python -m subunit.run discover -s burn_in_tests_poc_tempest_plugin.tests.api.compute | subunit-1to2 | subunit2csv 
+python -m subunit.run discover -s burn_in_tests_poc_tempest_plugin.tests.api.compute | subunit-1to2 | subunit2csv > tempest_output
